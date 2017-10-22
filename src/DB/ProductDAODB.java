224d0e47967ba97575eff2880b502a3c9211a49d
Igor Nascimento <igornascimento@gmail.com>
2017-10-22 18:02:09 +0000

    Save method

--- src/DB/ProductDAODB.java
+++ src/DB/ProductDAODB.java
@@ -4,6 +4,7 @@ import DAO.ProductDAO;
 import com.mysql.jdbc.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
@@ -45,6 +46,19 @@ public class ProductDAODB implements ProductDAO {
         try {
             String sql = "INSERT INTO product (name, description, quantity, price, unit) VALUES (?, ?, ?, ?, ?)";
             connectGettingId(sql);
+            command.setString(1, product.getName());
+            command.setString(2, product.getDescription());
+            command.setInt(3, product.getQuantity());
+            command.setDouble(4, product.getValue());
+            command.setString(5, product.getUnityOfMeasure());
+            command.executeUpdate();
+            // get the id
+            ResultSet result = command.getGeneratedKeys();
+            if (result.next()) {
+                id = result.getInt(1);
+            } else {
+                throw new SQLException();
+            }
         } catch (SQLException ex) {
             Logger.getLogger(ProductDAODB.class.getName()).log(Level.SEVERE, null, ex);
         }
